73725823da498d2b4194a3a322f47206ea013c88
pqhungbk <85327743+pqhungbk@users.noreply.github.com>
2021-06-05 11:28:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'My à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Start',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy. Cho anh hỏi nhá: Em có thích anh không? ',
+    btnYes: 'Vâng, Em là thích anh <33',
+    btnNo: 'Không, Anh là tầm trung thôi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Nhưng anh còn trẻ lắm phải suy nghĩ đã 😘😘',
+    messDesc: 'Còn một điều là anh muốn nghe giọng em nói',
+    btnAccept: 'Điều cuối cùng này <3',
+    messLink: './square.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
